063019caa0b3181dbb373327fe3b180d5c21eaa6
Rita Pinho <rpinho@alternativeairlines.com>
2022-08-02 18:02:18 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+
+import { HomeScreen } from "./screens/HomeScreen";
+import { MapScreen } from "./screens/MapScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { store } from "./store";
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView 
+          behavior={Platform.OS === "ios" ? "padding" : "height"} 
+          style={{flex:1}}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+              headerShown: false,
+            }}/>
+             <Stack.Screen name="MapScreen" component={MapScreen} options={{
+              headerShown: false,
+            }}/>
+          </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
